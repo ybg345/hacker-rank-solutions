9f8c82a83f833ef7407cd4552ee9a850cd40aa10
ybg345 <mehedi.farazi33@gmail.com>
2019-07-10 12:40:23 +0600

    Added 05_Pointers in C.c file

---
+++ C/00_Introduction/05_Pointers in C.c
@@ -0,0 +1,35 @@
+// https://www.hackerrank.com/challenges/pointer-in-c/problem
+// ----------------------------------------------------------
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int temp = *a;
+    *a = *a + *b;
+    *b = abs(temp - *b);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
+
+
+
+
+// Liked this method very much: 
+void update(int *a,int *b) {
+    // Complete this function
+    *a = *a + *b;
+    *b = abs(*a - 2*(*b));
+}
