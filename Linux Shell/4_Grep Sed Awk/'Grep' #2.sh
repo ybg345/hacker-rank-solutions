515a43ff9ff168b6cc15337133e0fc183ee07dc3
ybg345 <mehedi.farazi33@gmail.com>
2019-07-23 23:58:22 +0600

    Added Grep Sed Awk Folder

---
+++ Linux Shell/4_Grep Sed Awk/'Grep' #2.sh
@@ -0,0 +1,10 @@
+# Problem Link: https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-2/problem
+# -------------------------------------------------------------------------------------------------------
+
+
+grep "the " -i
+
+
+
+# Another Solution:
+# grep -i '\bthe\b'             # \b denotes word boundaries.
---
+++ Linux Shell/4_Grep Sed Awk/'Sed' command #2.sh
@@ -0,0 +1,5 @@
+# Problem Link: https://www.hackerrank.com/challenges/text-processing-in-linux-the-sed-command-2/problem
+# ------------------------------------------------------------------------------------------------------
+
+
+sed 's/thy/your/Ig'
